817c6b5ff0025512458e4e35a307d63009c14ba1
thuy-vq <thuy.vq@hotmail.com>
2018-11-21 21:49:56 +0700

    refactoring sign up component

---
+++ src/services/auth.js
@@ -0,0 +1,36 @@
+import axios from 'axios';
+import { validateAll } from 'indicative';
+import config from '../config';
+
+export default class AuthService {
+  async registerUser(data) {
+    const rules = {
+    name: 'required|string',
+    email: 'required|email',
+    password: 'required|string|min:6|confirmed'
+    };
+    const messages = {
+    required: 'The {{ field }} is required.',
+    'email.email': 'The email is invalid.',
+    'password.confirmed': 'The password confirmation does not match.'
+    }
+
+    try {
+      await validateAll(data, rules, messages)
+      const response = await axios.post(`${config.apiUrl}/auth/register`, {
+        name: data.name,
+        email: data.email,
+        password: data.password
+      })
+      return response.data.data;
+    } catch (errors) {
+      const formattedErrors = {};
+      if (errors.status === 422) {
+        formattedErrors['email'] = errors.response.data['email'][0];
+        return Promise.reject(formattedErrors);
+      }
+      errors.forEach(error => formattedErrors[error.field] = error.message);
+      return Promise.reject(formattedErrors);
+    }
+  }
+}
